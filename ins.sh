98a2c3e82d8c636e002bb4b32a6f351a7e4d7655
zzl <zzl@zzl-HZW>
2024-03-16 17:44:30 +0800

    add codon

--- ins.sh
+++ ins.sh
@@ -1,17 +1,20 @@
 export MAMBARC=$PWD/.mambarc
-if [[ $SHELL == "/bin/zsh" ]]; then 
+if [[ $SHELL == "/bin/zsh" ]]; then
 	pref=$HOME/miniforge-pypy3
 else
 	pref=$PWD/miniforge-pypy3
 fi
 
 if [[ -e "Miniforge-pypy3-$(uname)-$(uname -m).sh" ]]; then
-bash Miniforge-pypy3-$(uname)-$(uname -m).sh -b -p $pref
+	bash Miniforge-pypy3-$(uname)-$(uname -m).sh -b -p $pref
 else
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge-pypy3-$(uname)-$(uname -m).sh"
-bash Miniforge-pypy3-$(uname)-$(uname -m).sh -b -p $pref
+	curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge-pypy3-$(uname)-$(uname -m).sh"
+	bash Miniforge-pypy3-$(uname)-$(uname -m).sh -b -p $pref
 fi
 
-source $pref/bin/activate 
+# codon
+/bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
+
+source $pref/bin/activate
 python install_tools.py
 cp zshrc .zshrc
