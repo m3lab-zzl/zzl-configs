0ba0854691aa7f6a95c39bc9f50155fad796b3a5
deelin <deelin@zzls-MacBook-Air.local>
2024-03-19 10:31:40 +0800

    add fl to make fortran syntax highlighting right

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- bind <space>fl to trigger fortran highlighting setting
+-- :let b:fortran_fixed_source=0
+-- :set syntax=fortran
+vim.api.nvim_set_keymap('n', '<leader>fl', ':let b:fortran_fixed_source=0<CR>:set syntax=fortran<CR>', {noremap = true})
