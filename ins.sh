7e47d4af3099cfb0965b9d5128e699b91de4a2c7
zzl <zzl@zzl-HZW>
2024-03-22 15:30:24 +0800

    add entr tool

--- ins.sh
+++ ins.sh
@@ -5,15 +5,25 @@ else
 	pref=$PWD/miniforge-pypy3
 fi
 
-if [[ -e "Miniforge-pypy3-$(uname)-$(uname -m).sh" ]]; then
-	bash Miniforge-pypy3-$(uname)-$(uname -m).sh -b -p $pref
-else
-	curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge-pypy3-$(uname)-$(uname -m).sh"
-	bash Miniforge-pypy3-$(uname)-$(uname -m).sh -b -p $pref
+# check if mamba is found
+if ! command -v mamba &>/dev/null; then
+	if [[ -e "Miniforge-pypy3-$(uname)-$(uname -m).sh" ]]; then
+		bash Miniforge-pypy3-$(uname)-$(uname -m).sh -b -p $pref
+	else
+		curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge-pypy3-$(uname)-$(uname -m).sh"
+		bash Miniforge-pypy3-$(uname)-$(uname -m).sh -b -p $pref
+	fi
 fi
 
-# codon
-/bin/bash -c "$(curl -fsSL https://exaloop.io/install.sh)"
+# check if entr is found
+if ! command -v entr &>/dev/null; then
+	tar xf entr-5.5.tar.gz
+	cd entr-5.5
+	./configure
+	make test
+	PREFIX=$PWD/entr make install
+	rm -rf entr-5.5
+fi
 
 source $pref/bin/activate
 python install_tools.py
