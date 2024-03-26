20bc7eb646d7dc27d82346ad2745ea90913259b1
zzl <zzl@zzl-HZW>
2024-03-26 10:55:18 +0800

    disable mouse for ssh

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- disable mouse to enable mouse copy over ssh
+vim.opt.mouse = ""
 
 if vim.g.vscode then
   print("vscode do not need theme")
